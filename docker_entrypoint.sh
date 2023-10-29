d6807b2b0632ae3dc19efbec43682c976d82501c
Eric P <ericpp@gmail.com>
2023-10-29 15:26:04 -0500

    Fixed `ADMIN_PASS` environment var

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 export ADMIN_USER=$(yq .username < /data/start9/config.yaml)
-export ADMIN_PASSWORD=$(yq .password < /data/start9/config.yaml)
+export ADMIN_PASS=$(yq .password < /data/start9/config.yaml)
 
 exec pm2-runtime server/index.js
